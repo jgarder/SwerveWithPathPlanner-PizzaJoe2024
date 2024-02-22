4380c79f9644ef9920e31a1d7b564a5051853d57
jgarder <jgarder@gmail.com>
2024-02-22 10:06:42 -0500

    asdf

--- src/main/java/frc/robot/subsystems/DrivetrainManager.java
+++ src/main/java/frc/robot/subsystems/DrivetrainManager.java
@@ -14,7 +14,7 @@ import frc.robot.generated.TunerConstants;
 public class DrivetrainManager {
     
     
-  private double MaxSpeed = 6; // 6 meters per second desired top speed
+  private double MaxSpeed = 9;//6; // 6 meters per second desired top speed
   private double MaxAngularRate = 1.8 * Math.PI; // 3/4 of a rotation per second max angular velocity
 
   
